b4f18c01a241ac5a7cd6af63fd14e60c50ce2519
Akash <99742784+Akash-Shilimkar@users.noreply.github.com>
2024-01-21 15:07:24 +0530

    Java Program to print Star_0_Pattern.
    Output 
    
    * 0 * 0 
    * 0 * 0 
    * 0 * 0 
    * 0 * 0

---
+++ Patten Prgram/Star_0_Pattern1.java
@@ -0,0 +1,19 @@
+class Star_0_Pattern1{
+    public static void main(String[] args){
+        for(int i=1;i<=4;i++)
+        {
+            for(int j=1;j<=4;j++)
+            {
+                if(j%2==0)
+                {
+                    System.out.print("0"+" ");
+                }
+                else
+                {
+                    System.out.print("*"+" ");
+                }
+            }
+            System.out.println( );
+        }
+    }
+}
